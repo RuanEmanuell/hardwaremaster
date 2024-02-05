5f43c48ac41ca3bd0a46e0473e15947f185b12cb
Ruan Emanuell Alencarr <ruanvitorcom540@gmail.com>
2024-02-05 16:44:51 -0300

    (Version 1.0) Configuring the basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cpuList, setCpuList] = useState(null);
+  const [cpuName, setCpuName] = useState('');
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  async function fetchApi() {
+    try {
+      const response = await fetch('http://localhost:3001/teste');
+      const data = await response.json();
+      setCpuList(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function createCpu() {
+    try {
+      const response = await fetch('http://localhost:3001/post', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: cpuName, brand: 'AMD', launchDate: '2019', generation: '3000' })
+      });
+      setCpuName('');
+      fetchApi();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function editCpu(id) {
+    try {
+      const response = await fetch(`http://localhost:3001/update/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: 'teste', brand: 'AMD', launchDate: '2019', generation: '3000' })
+      });
+      setCpuName('');
+      fetchApi();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function deleteCpu(id) {
+    try {
+      const response = await fetch(`http://localhost:3001/delete/${id}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' }
+      });
+      setCpuName('');
+      fetchApi();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+
+  const HandleChange = (event) => {
+    setCpuName(event.target.value);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {cpuList ? <>
+        {cpuList.map((cpu) =>
+        <div>
+          <h1 key={cpu.id}>{cpu['name']}</h1>
+          <button onClick={()=>editCpu(cpu['_id'])}>Editar</button>
+          <button onClick={()=>deleteCpu(cpu['_id'])}>Deletar</button>
+          </div>
+        )}
+      </> : <></>}
+      <br></br>
+      <input value={cpuName} onChange={HandleChange}></input>
+      <input></input>
+      <input></input>
+      <input></input>
+      <button onClick={createCpu}>Postar dados</button>
     </div>
   );
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import mongoose, { mongo } from 'mongoose';
+import bodyParser from 'body-parser';
 import fs from 'fs';
+import cors from 'cors';
 import Cpu from '../src/models/cpu.js';
 
 const app = express();
 const port = 3001;
-
 const mongoPasswordPath = '../server/confidential/mongopassword.txt';
-
 const uri = fs.readFileSync(mongoPasswordPath, 'utf8');
+
+app.use(cors());
+app.use(bodyParser.json());
 
 mongoose.connect(uri);
 
@@ -19,17 +22,41 @@ db.once('open', () => {
     console.log('Conectado ao MongoDB');
 });
 
-const newCpu = new Cpu({
-    name: 'Ryzen 5 5600',
-    brand: 'AMD',
-    launchDate: '2020',
-    generation: '5000'
-})
+app.get('/teste', async (req, res) => {
+    const cpus = await db.collection('cpus').find({}).toArray();
+    res.json(cpus);
+    console.log(cpus);
+});
 
-newCpu.save();
+app.post('/post', async (req, res) => {
+    try {
+        const newCpu = new Cpu(req.body);
+        await newCpu.save();
+        res.sendStatus(201)
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500)
+    }
+});
 
-app.get('/teste', (req, res) => {
-    res.send('teste');
+app.put('/update/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const updatedCpu = await Cpu.findByIdAndUpdate(id, req.body, {new: true});
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.delete('/delete/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const updatedCpu = await Cpu.findByIdAndDelete(id, req.body, {new: true});
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.listen(port, () => {
