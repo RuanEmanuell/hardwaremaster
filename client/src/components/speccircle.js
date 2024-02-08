3a65a6808a25fcbbddf68bb20e43aa1a38ebdb56
Ruan Emanuell Alencarr <ruanvitorcom540@gmail.com>
2024-02-08 16:38:22 -0300

    (Version 1.0) Adjusting responsivity

--- client/src/components/speccircle.js
+++ client/src/components/speccircle.js
@@ -3,10 +3,10 @@ import './components.css'
 function SpecCircle(props) {
     let specCircleColor = 'rgb(0, 182, 0)';
 
-    if (props.performanceRating <= 60) {
+    if (props.performanceRating <= 25) {
+        specCircleColor = 'red';
+    }else if (props.performanceRating <= 60) {
         specCircleColor = 'rgb(245, 228, 0)';
-    } else if (props.performanceRating <= 25) {
-        specCircleColor = 'rgb(196, 7, 0)';
     }
 
     return (
