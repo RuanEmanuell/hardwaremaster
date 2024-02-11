5089375e0b19c6c951da25d1e8730cef48d842f1
Ruan Emanuell Alencarr <ruanvitorcom540@gmail.com>
2024-02-11 20:37:24 -0300

    (Version 1.0) Optimizing code

--- client/src/App.js
+++ client/src/App.js
@@ -8,41 +8,8 @@ import OrangeButton from './components/orangebutton';
 function App() {
   const [fullList, setFullList] = useState(null);
   const [editingPartId, setEditingPartId] = useState(null);
-  const [cpuData, setCpuData] = useState({
-    cpuName: '',
-    cpuBrand: '',
-    cpuLaunch: '',
-    cpuGeneration: '',
-    cpuCores: '',
-    cpuThreads: '',
-    cpuFrequency: '',
-    cpuPrice: '',
-    cpuLink: '',
-    cpuLink2: '',
-    cpuLink3: '',
-    imageLink: '',
-    cpuIgpu: false,
-    cpuPerformance: '',
-    igpuPerformance: '',
-    costBenefit: '',
-  });
-  const [gpuData, setGpuData] = useState({
-    gpuName: '',
-    gpuBrand: '',
-    gpuLaunch: '',
-    gpuGeneration: '',
-    gpuCores: '',
-    gpuMemory: '',
-    gpuMemoryType: '',
-    gpuMemoryBus: '',
-    shopLink: '',
-    shopLink2: '',
-    shopLink3: '',
-    imageLink: '',
-    gpuPrice: '',
-    gpuPerformance: '',
-    costBenefit: '',
-  });
+  const [cpuData, setCpuData] = useState(getInitialPartData('cpu'));
+  const [gpuData, setGpuData] = useState(getInitialPartData('gpu'));
   const [isSelectingType, setSelecting] = useState('none');
   const [isAdding, setAdding] = useState('none');
   const [selectedPartType, setSelectedType] = useState('cpu');
@@ -51,52 +18,52 @@ function App() {
     fetchApi();
   }, []);
 
-  function clearInputs() {
-    setCpuData({
-      ...cpuData,
-      cpuName: '',
-      cpuBrand: '',
-      cpuLaunch: '',
-      cpuGeneration: '',
-      cpuCores: '',
-      cpuThreads: '',
-      cpuFrequency: '',
-      cpuPrice: '',
-      cpuLink: '',
-      cpuLink2: '',
-      cpuLink3: '',
-      imageLink: '',
-      cpuIgpu: false,
-      cpuPerformance: '',
-      igpuPerformance: '',
-      costBenefit: '',
-    });
-    setGpuData({
-      ...gpuData,
-      gpuName: '',
-      gpuBrand: '',
-      gpuLaunch: '',
-      gpuGeneration: '',
-      gpuCores: '',
-      gpuMemory: '',
-      gpuMemoryType: '',
-      gpuMemoryBus: '',
-      shopLink: '',
-      shopLink2: '',
-      shopLink3: '',
-      imageLink: '',
-      gpuPrice: '',
-      gpuPerformance: '',
-      costBenefit: '',
-    })
+  function getInitialPartData(type) {
+    return {
+      ...(type === 'cpu'
+        ? {
+            cpuName: '',
+            cpuBrand: '',
+            cpuLaunch: '',
+            cpuGeneration: '',
+            cpuCores: '',
+            cpuThreads: '',
+            cpuFrequency: '',
+            cpuLink: '',
+            cpuLink2: '',
+            cpuLink3: '',
+            imageLink: '',
+            cpuIgpu: false,
+            cpuPerformance: '',
+            igpuPerformance: '',
+            costBenefit: '',
+          }
+        : {
+            gpuName: '',
+            gpuBrand: '',
+            gpuLaunch: '',
+            gpuGeneration: '',
+            gpuCores: '',
+            gpuMemory: '',
+            gpuMemoryType: '',
+            gpuMemoryBus: '',
+            shopLink: '',
+            shopLink2: '',
+            shopLink3: '',
+            imageLink: '',
+            gpuPerformance: '',
+            costBenefit: '',
+          }),
+    };
   }
 
-  function selectingPartType() {
-    if (isSelectingType == 'none') {
-      setSelecting('block');
-    } else {
-      setSelecting('none');
-    }
+  function clearInputs() {
+    setCpuData(getInitialPartData('cpu'));
+    setGpuData(getInitialPartData('gpu'));
+  }
+
+  function toggleSelecting() {
+    setSelecting((prevState) => (prevState === 'none' ? 'block' : 'none'));
   }
 
   async function fetchApi() {
@@ -111,45 +78,26 @@ function App() {
 
   async function createOrEditPart() {
     const isCpu = selectedPartType === 'cpu';
-    const dataToSend = isCpu ? {
-      type: selectedPartType,
-      ...cpuData
-    } : {
-      type: selectedPartType,
-      ...gpuData
-    };
+    const dataToSend = isCpu ? { type: selectedPartType, ...cpuData } : { type: selectedPartType, ...gpuData };
 
-    if (editingPartId) {
-      try {
-        const response = await fetch(`http://localhost:3001/update/${editingPartId}`, {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(dataToSend),
-        });
-        clearInputs();
-        setEditingPartId(null);
-        fetchApi();
-      } catch (err) {
-        console.log(err);
-      }
-    } else {
-      try {
-        const response = await fetch('http://localhost:3001/post', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(dataToSend),
-        });
-        clearInputs();
-        fetchApi();
-      } catch (err) {
-        console.log(err);
-      }
+    try {
+      const response = await fetch(editingPartId ? `http://localhost:3001/update/${editingPartId}` : 'http://localhost:3001/post', {
+        method: editingPartId ? 'PUT' : 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dataToSend),
+      });
+
+      clearInputs();
+      setEditingPartId(null);
+      fetchApi();
+      showModalAddPart();
+    } catch (err) {
+      console.log(err);
     }
-    showModalAddPart();
   }
 
   function editPart(index) {
-    let currentPart = fullList[index];
+    const currentPart = fullList[index];
     setEditingPartId(currentPart['_id']);
     showModalAddPart(currentPart['type']);
 
@@ -162,10 +110,11 @@ function App() {
 
   async function deletePart(id) {
     try {
-      const response = await fetch(`http://localhost:3001/delete/${id}`, {
+      await fetch(`http://localhost:3001/delete/${id}`, {
         method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' }
+        headers: { 'Content-Type': 'application/json' },
       });
+
       clearInputs();
       fetchApi();
     } catch (err) {
@@ -174,53 +123,45 @@ function App() {
   }
 
   async function updatePrice(id) {
-    let newPrice = '';
-    const response = await fetch(`http://localhost:3001/currentprice/${id}`);
-    const data = await response.json();
-    newPrice = data['preço'];
     try {
-      const response = await fetch(`http://localhost:3001/update/${id}`, {
+      const response = await fetch(`http://localhost:3001/currentprice/${id}`);
+      const data = await response.json();
+      const newPrice = data['preço'];
+
+      await fetch(`http://localhost:3001/update/${id}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ price: newPrice })
-      })
+        body: JSON.stringify({ price: newPrice }),
+      });
+
       fetchApi();
     } catch (error) {
-
+      console.log(error);
     }
   }
 
-  const HandleChange = (event) => {
+  const handleChange = (event) => {
     const { name, value, type, checked } = event.target;
+    const data = selectedPartType === 'cpu' ? cpuData : gpuData;
 
     if (selectedPartType === 'cpu') {
-      setCpuData({
-        ...cpuData,
-        [name]: type === 'checkbox' ? checked : value,
-      });
+      setCpuData({ ...data, [name]: type === 'checkbox' ? checked : value });
     } else if (selectedPartType === 'gpu') {
-      setGpuData({
-        ...gpuData,
-        [name]: type === 'checkbox' ? checked : value,
-      });
+      setGpuData({ ...data, [name]: type === 'checkbox' ? checked : value });
     }
   };
 
   function showModalAddPart(partType) {
     clearInputs();
     setSelectedType(partType);
-    selectingPartType();
-    if (isAdding == 'none') {
-      setAdding('block');
-    } else {
-      setAdding('none');
-    }
+    toggleSelecting();
+    setAdding((prevState) => (prevState === 'none' ? 'block' : 'none'));
   }
 
   const partTypeDataMap = {
-    'cpu': cpuData,
-    'gpu': gpuData,
-  };
+    cpu: cpuData,
+    gpu: gpuData,
+  }
 
   return (
     <div className="mainContainer">
@@ -293,7 +234,7 @@ function App() {
             <h3 className="partType" onClick={() => showModalAddPart('ssd')}>SSD</h3>
             <h3 className="partType" onClick={() => showModalAddPart('case')}>GABINETE</h3>
           </div>
-          <button className="addPart" onClick={selectingPartType}><h1>+</h1></button>
+          <button className="addPart" onClick={toggleSelecting}><h1>+</h1></button>
         </div>
         <div style={{ display: isAdding }} className="addPartContainer">
           <div className="addPartModal">
@@ -313,7 +254,7 @@ function App() {
                   placeholder={key}
                   key={key}
                   name={key}
-                  onChange={(event) => HandleChange(event)}
+                  onChange={(event) => handleChange(event)}
                   value={gpuData[key]}></input> </>: <></>)}
               </div>
               <div class="saveButtonContainer">
