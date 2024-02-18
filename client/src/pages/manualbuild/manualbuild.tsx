0a1b0690c47520ba31cdd72a4a616e70746e4b4d
Ruan Emanuell Alencarr <ruanvitorcom540@gmail.com>
2024-02-18 14:19:31 -0300

    (Version 1.0) Changing manual build functionality

--- client/src/pages/manualbuild/manualbuild.tsx
+++ client/src/pages/manualbuild/manualbuild.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import mbStyle from './styles/manualbuild.module.css';
 import NavBar from '../../components/global/navbar';
 import CpuIcon from '../../images/cpu.png';
+import StandartButton from '../../components/global/standartbutton';
 
 interface Part {
   type: string;
@@ -13,6 +14,7 @@ interface Part {
   cpuCores: string;
   cpuThreads: string;
   cpuFrequency: string;
+  cpuSocket: string;
 }
 
 interface Cpu {
@@ -22,6 +24,7 @@ interface Cpu {
   cpuThreads: string;
   cpuFrequency: string;
   cpuImageLink: string;
+  cpuSocket: string;
   cpuPrice: string;
 }
 
@@ -29,6 +32,7 @@ const ManualBuild: React.FC = () => {
   const [cpuList, setCpuList] = useState<Cpu[]>([]);
   const [selectedCPU, setSelectedCPU] = useState<Cpu | null>(null);
   const [selectCpuInput, setSelectCpuInput] = useState<string>('');
+  const [totalBuildPrice, setTotalBuildPrice] = useState<number>(0)
 
   async function getPartList() {
     try {
@@ -43,9 +47,10 @@ const ManualBuild: React.FC = () => {
               cpuBrand: data[i]['brand'],
               cpuCores: data[i]['cpuCores'],
               cpuFrequency: data[i]['cpuFrequency'],
-              cpuThreads: data[i]['cpuFrequency'],
+              cpuThreads: data[i]['cpuThreads'],
               cpuPrice: data[i]['price'],
-              cpuImageLink: data[i]['imageLink']
+              cpuImageLink: data[i]['imageLink'],
+              cpuSocket: data[i]['cpuSocket']
             });
         };
       }
@@ -57,6 +62,13 @@ const ManualBuild: React.FC = () => {
 
   function selectCPU(cpu: Cpu) {
     setSelectedCPU(cpu);
+    setTotalBuildPrice(totalBuildPrice + parseFloat(cpu['cpuPrice']))
+  }
+
+  function resetSelectedCPU(){
+    setTotalBuildPrice(totalBuildPrice - parseFloat(selectedCPU!['cpuPrice']));
+    setSelectedCPU(null);
+    setSelectCpuInput('');
   }
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -81,26 +93,52 @@ const ManualBuild: React.FC = () => {
             </div>
             <div className={mbStyle.partNameAndInputBox}>
               <h3>{selectedCPU ? `${selectedCPU['cpuBrand']} ${selectedCPU['cpuName']}` : 'Selecione um processador'}</h3>
-              <input className={mbStyle.partPickerInput}
-                placeholder='Digite o nome de um processador...'
-                value={selectCpuInput}
-                onChange={(event) => handleChange(event)}></input>
-              <div className={mbStyle.inputResultBox}>
-                {selectCpuInput !== '' ?
-                  <div>
-                    {cpuList.filter(cpu =>
-                      `${cpu['cpuBrand']} ${cpu['cpuName']}`.toLowerCase().trim().includes(selectCpuInput)).map(cpu =>
-                        <div className={mbStyle.inputPicker}>
-                        <h3 onClick={() => selectCPU(cpu)}>{cpu['cpuBrand']} {cpu['cpuName']}</h3>
-                        </div>
-                        )}
-                  </div>
-                  : <></>}
-              </div>
+              {!selectedCPU ?
+                <div>
+                  <input className={mbStyle.partPickerInput}
+                    placeholder='Digite o nome de um processador...'
+                    value={selectCpuInput}
+                    onChange={(event) => handleChange(event)}></input>
+                  {selectCpuInput !== '' ?
+                    <div className={mbStyle.inputResultBox}>
+                      <div>
+                        {cpuList.filter(cpu =>
+                          `${cpu['cpuBrand']} ${cpu['cpuName']}`.toLowerCase().trim().includes(selectCpuInput)).map(cpu =>
+                            <div className={mbStyle.inputPicker} onClick={() => selectCPU(cpu)}>
+                              <div className={mbStyle.inputPickerNameAndImg}>
+                                <img src={cpu['cpuImageLink']} className={mbStyle.inputImg}></img>
+                                <h3>{cpu['cpuBrand']} {cpu['cpuName']}</h3>
+                              </div>
+                              <h3 className={mbStyle.inputPrice}>R$ {cpu['cpuPrice']}</h3>
+                            </div>
+                          )}
+                      </div>
+                    </div>
+                    : <></>} </div>
+                : <div>
+                  <p>Núcleos: {selectedCPU['cpuCores']}, Threads: {selectedCPU['cpuThreads']}</p>
+                  <p>Frequência: {selectedCPU['cpuFrequency']} GHz</p>
+                  <p>Socket: {selectedCPU['cpuSocket']}</p>
+                </div>}
             </div>
-            <div></div>
-            <div></div>
+            <div>
+              {selectedCPU ?
+                <div>
+                  <div className={mbStyle.priceBox}>
+                    <h3>Preço:</h3>
+                    <p>R$ {selectedCPU['cpuPrice']}</p>
+                  </div>
+                  <div className={mbStyle.buttonBox}>
+                    <StandartButton
+                      buttonLabel='Trocar peça'
+                      onClick = {() => resetSelectedCPU()}
+                    />
+                  </div>
+                </div>
+                : <></>}
+            </div>
           </div>
+          <h2 className={mbStyle.buildPrice}>Preço total: R$ {totalBuildPrice}</h2>
         </main>
       </div>
     </div>
