f234186d96a7f82495179905658e03766b472153
Ruan Emanuell Alencarr <ruanvitorcom540@gmail.com>
2024-02-27 23:24:00 -0300

    (Version 1.0) Adding builds to profile

--- client/src/pages/profile/profile.tsx
+++ client/src/pages/profile/profile.tsx
@@ -3,11 +3,11 @@ import profileStyle from './styles/profile.module.css';
 import { useAuth } from '../../utils/auth';
 
 const Profile: React.FC = () => {
-  const {currentUser} = useAuth();
+  const { currentUser } = useAuth();
   const [userBuilds, setUserBuildList] = useState<any[] | undefined>(undefined);
   const [userParts, setUserPartList] = useState<any[] | undefined>(undefined);
 
-  async function getUserBuildList(){
+  async function getUserBuildList() {
     try {
       const response = await fetch('http://localhost:3001/builds/builds');
       const builds: any[] = await response.json();
@@ -18,13 +18,12 @@ const Profile: React.FC = () => {
     }
   }
 
-  async function getPartList(){
+  async function getPartList() {
     try {
-      if(userBuilds){
-      const response = await fetch('http://localhost:3001/list/parts');
-      const parts: any[] = await response.json();
-      const userPartsList = parts.filter(part => part._id === userBuilds[0].gpuId);
-      setUserPartList(userPartsList);
+      if (userBuilds) {
+        const response = await fetch('http://localhost:3001/list/parts');
+        const parts: any[] = await response.json();
+        setUserPartList(parts);
       }
     } catch (err) {
       console.log(err);
@@ -40,9 +39,30 @@ const Profile: React.FC = () => {
   }, [userBuilds]);
 
   return (
-      <div>
-        {userParts && userParts.length > 0 ? userParts[0].name : ''}
-      </div>
+    <main style = {{display: 'grid', gridTemplateColumns: '1fr 1fr'}}>
+      {userBuilds?.map
+        ((build, index) => (
+          <div style = 
+          {{ border: '2px solid red', margin: '5% 0'}}>
+            <h2>Build {index+1}</h2>
+            <div style = {{display: 'grid', gridTemplateColumns: '1fr 1fr'}}>
+            {userParts?.filter
+              (part => (part._id === (
+                part.type === 'cpu' ? build.cpuId :
+                  part.type === 'gpu' ? build.gpuId :
+                    part.type === 'mobo' ? build.moboId
+                      : part.type === 'ram' ? build.ramId
+                      : part.type === 'power' ? build.powerId
+                      : part.type === 'ssd' ? build.ssdId
+                      : build.caseId))).map
+              (part => 
+                <div>
+                  <img src = {part.imageLink} style = {{width: '10%'}}></img>
+                  <h3>{part.name}</h3>
+                  <h3>R$ {part.price}</h3>
+                </div>
+              )}</div></div>))}
+    </main>
   )
 }
 
