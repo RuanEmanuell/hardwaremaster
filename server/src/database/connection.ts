3d16607abd9997f6e2ad07360cdd3c6772f6619d
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2024-03-13 11:14:43 -0300

    (Version 1.0) Adding vercel config file

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -2,12 +2,8 @@ import mongoose, { Connection } from 'mongoose';
 
 require('dotenv').config();
 
-const uri = process.env.MONGO_PASSWORD!;
-
-mongoose.connect(uri);
+mongoose.connect(process.env.MONGO_PASSWORD!);
 
 const db : Connection = mongoose.connection;
-
-console.log(process.env.MONGO_PASSWORD!);
 
 export default db;
