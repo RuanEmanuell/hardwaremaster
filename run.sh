52f7d9aef8e6b9a1af3084d939e9d1ddd2de6a27
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2024-04-03 12:27:41 -0300

    (Version 1.0) Fixing minor css bugs

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 cd client
 npm install
-code .
 gnome-terminal -- sudo npm start
 
 sleep 2
 
 cd ../server
 npm install
-code .
 gnome-terminal -- npm run dev
+
+cd ../client
+code .
+
+cd ../server
+code .
